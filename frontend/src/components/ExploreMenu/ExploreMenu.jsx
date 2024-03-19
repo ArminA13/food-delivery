a402d82e6c64fe27979bcdce67ab592e61971f4c
ArmineMuradyan <muradyan.armin13@gmail.com>
2024-03-19 23:22:37 +0400

    Add new components and other changes

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,22 +2,23 @@ import React from 'react';
 import './ExploreMenu.css';
 import { menu_list } from '../../assets/assets';
 
-const ExploreMenu = () => {
+const ExploreMenu = ({category, setCategory}) => {
   return (
     <div className='explore-menu' id='explore-menu'>
         <h1>Explore Our Menu</h1>
         <p className='explore-menu-text'>Welcome to The Biggest Network of Food Ordering & Delivery</p>
-        <div className="explore-menu-list"></div>
-        {menu_list.map((item, index)=> {
+        <div className="explore-menu-list">
+            {menu_list.map((item, index)=> {
             return(
-                <div key={index} className='explore-menu-list-item'>
-                    <img src={item.menu_image}/>
+                <div onClick={() => setCategory(prev => prev===item.menu_name?"All":item.menu_name)} key={index} className='explore-menu-list-item'>
+                    <img className={category===item.menu_name?"active":""} src={item.menu_image}/>
                     <p>{item.menu_name}</p>
 
                 </div>
             )
         })}
-      
+        </div>
+        <hr/>
     </div>
   )
 }
